00a85952534647c7dbb87f9608496084fc07dc94
msavoiu <savoiumadeline@gmail.com>
2025-04-21 17:07:52 -0700

    Create searchbar

---
+++ react-app/src/components/searchbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+
+function SearchBar() {
+    const [searchQuery, setSearchQuery] = useState("");
+    
+    const onChange = (e) => {
+        e.preventDefault();
+        setSearchQuery(e.target.value);
+    };
+      
+    const onSubmit = (e) => {
+        // Logic to handle the search and query the database for products that fulfill it
+    };
+      
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <input 
+                    onSubmit={onSubmit}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="Search"
+                    required
+                />
+            </form>
+        </>
+    );
+}
+
+export default SearchBar;
